5d7fa61e4c5754a3c4972eb99341b16a28b3bf1e
circius <circius@posteo.de>
2020-02-27 14:11:05 +0000

    Fixed broken test and configured tox as a test-runner. All green.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,7 @@
 import blrequests.cli
 import subprocess
 
-def test_cli():
-    def test_bare_usage_stdout():
-        result=subprocess.run(["blrequests"])
-        expected="Hello World!"
-        assert(result == expected)
+def test_bare_usage_stdout():
+    result=subprocess.run(["blrequests"], capture_output=True)
+    expected=b"Hello World!"
+    assert(result.stdout.strip() == expected)
