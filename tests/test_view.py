ca972c4c459c29c15e5f704ec3d1ffad18eb1abf
circius <circius@posteo.de>
2020-02-28 13:55:20 +0000

    Removed tests that introduced a dependecy of the view functions on the app functions.

--- tests/test_view.py
+++ tests/test_view.py
@@ -8,13 +8,11 @@ def test_tabulates_empty_table():
 
 def test_tabulates_isou_1row_table():
     isou_tabulated = view.tabulate_bltable(testdata.isou_1row_table)
-    assert len(isou_tabulated.split("\n")) == 3  # three lines
     assert "ISOU" in isou_tabulated  # has author's surname
     assert "author" in isou_tabulated  # has keys as headers
 
 
 def test_tabulates_isou_1row_table_headerless():
     isou_tabulated = view.tabulate_bltable(testdata.isou_1row_table, headers=False)
-    assert len(isou_tabulated.split("\n")) == 3  # three lines
     assert "ISOU" in isou_tabulated  # has author's surname
     assert "author" not in isou_tabulated  # has keys as headers
