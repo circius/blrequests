dd02b4522041c00bdcf4ef36af5c37ead9b0ecf2
circius <circius@posteo.de>
2020-02-28 10:54:52 +0000

    Factored out data definitions into their own submodule.

---
+++ src/blrequests/data_definitions.py
@@ -0,0 +1,22 @@
+""" data definitions for blrequests
+"""
+from typing import NewType
+
+""" A BLTABLE is a [List-of BLROW]
+"""
+
+BlTable = NewType("BlTable", list)
+
+
+""" A BLROW is a Dictionary with the following compulsory entries:
+        'title' -> STRING
+        'author' -> STRING
+        'shelfmark' -> STRING
+        'year' -> STRING
+        'volume' -> STRING
+        'notes' -> STRING
+        'status' -> STRING
+        'deliver to' -> STRING
+        'date required' -> STRING
+"""
+BlRow = NewType("BlRow", dict)
--- src/blrequests/view.py
+++ src/blrequests/view.py
@@ -2,25 +2,9 @@
 bltables.
 
 """
+from blrequests.data_definitions import BlTable, BlRow
 from tabulate import tabulate
 from typing import NewType
-
-""" A BLTABLE is a [List-of BLROW]
-"""
-
-BlTable = NewType("BlTable", list)
-
-
-""" A BLROW is a Dictionary with the following compulsory entries:
-        'title' -> STRING
-        'author' -> STRING
-        'shelfmark' -> STRING
-        'year' -> STRING
-        'status' -> STRING
-        'deliver_to' -> STRING
-        'date_required' -> STRING
-"""
-BlRow = NewType("BlRow", dict)
 
 
 def tabulate_table(
